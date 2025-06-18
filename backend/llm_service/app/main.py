0a88c2c16a0e19b2d995f60df395e0a75c1ef172
Prakhar-Kumar-1314 <prakharkumar1314@gmail.com>
2025-06-18 20:26:10 +0530

    cors is absolute shit

--- backend/llm_service/app/main.py
+++ backend/llm_service/app/main.py
@@ -23,6 +23,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:8080",
         "http://localhost:5173",
+        "https://t3-chat-coral.vercel.app/",
         "https://t3-chat-coral.vercel.app/"
     ],
     allow_credentials=True,
